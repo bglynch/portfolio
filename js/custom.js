325f7b01b7278a25417199574edbed9bf56de2e6
Brian Lynch <bglynch17@gmail.com>
2018-10-13 19:26:38 +0000

    file created for custom javascript: js/custom.js

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+/*
+Function to resize the navbar on scroll
+*/
+function resizeHeaderOnScroll() {
+  const distanceY = window.pageYOffset || document.documentElement.scrollTop,
+  shrinkOn = 250,
+  headerEl = document.getElementById('navbar');
+  
+  if (distanceY > shrinkOn) {
+    headerEl.classList.add("scrolled-navbar");
+  } else {
+    headerEl.classList.remove("scrolled-navbar");
+  }
+}
+window.addEventListener('scroll', resizeHeaderOnScroll);
--- js/init.js
+++ js/init.js
@@ -6,16 +6,3 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
-
-function resizeHeaderOnScroll() {
-  const distanceY = window.pageYOffset || document.documentElement.scrollTop,
-  shrinkOn = 250,
-  headerEl = document.getElementById('navbar');
-  
-  if (distanceY > shrinkOn) {
-    headerEl.classList.add("scrolled-navbar");
-  } else {
-    headerEl.classList.remove("scrolled-navbar");
-  }
-}
-window.addEventListener('scroll', resizeHeaderOnScroll);
