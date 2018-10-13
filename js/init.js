c43cb02a76f96ad04901681f04772ca549bbd935
Brian Lynch <bglynch17@gmail.com>
2018-10-13 18:03:57 +0000

    added scroll js to navbar

--- js/init.js
+++ js/init.js
@@ -6,3 +6,16 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+function resizeHeaderOnScroll() {
+  const distanceY = window.pageYOffset || document.documentElement.scrollTop,
+  shrinkOn = 250,
+  headerEl = document.getElementById('navbar');
+  
+  if (distanceY > shrinkOn) {
+    headerEl.classList.add("scrolled-navbar");
+  } else {
+    headerEl.classList.remove("scrolled-navbar");
+  }
+}
+window.addEventListener('scroll', resizeHeaderOnScroll);
