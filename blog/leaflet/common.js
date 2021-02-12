800a7d1303546353c5195ac04c81390b2d1b9e28
bglynch <bglynch17@gmail.com>
2021-02-12 15:35:08 +0000

    added example with modified circle markers, done my modifying the source code

--- blog/leaflet/common.js
+++ blog/leaflet/common.js
@@ -5,26 +5,36 @@ const config = {
 function createList() {
     html = `
     <h2>DOM Rendering Markers</h2>
-    <a href="./01-circleMarkers.html"><p class="h6">L.circleMarker</p></a>
-    <a href="./01-circle.html"><p class="h6">L.circle</p></a>
-    <a href="./02-svgOverlay.html"><p class="h6">L.svgOverlay</p></a>
-    <a href="./03-icon.html"><p class="h6">L.icon (png)</p></a>
-    <a href="./03.2-icon.html"><p class="h6">L.icon (svg to encoded base-64)</p></a>
-    <a href="./04-divIcon.html"><p class="h6">L.divIcon (custom)</p></a>
+    <h3>Will resize on zoom</h3>
+    <a href="./01-circleMarkers.html"><p>L.circleMarker</p></a>
+    <a href="./03-icon.html"><p>L.icon (png)</p></a>
+    <a href="./03.2-icon.html"><p>L.icon (svg to encoded base-64)</p></a>
+    <a href="./04-divIcon.html"><p>L.divIcon (custom)</p></a>
+    <h3>No resize on zoom</h3>
+    <a href="./01-circle.html"><p>L.circle</p></a>
+    <a href="./02-svgOverlay.html"><p>L.svgOverlay</p></a>
     <hr>
     <h2>Canvas Rendering Markers</h2>
-    <a href="./21-circle-markers.html"><p class="h6">L.circleMarker</p></a>
+    <a href="./21-circleMarker.html"><p>L.circleMarker, 10,000 markers</p></a>
+    <a href="./22-leafletMarkersCanvas.html"><p>Plugin: leaflet-markers-canvas, L.Icon (png)</p></a>
+    <a href="./22.1-leafletMarkersCanvas.html"><p>Plugin: leaflet-markers-canvas, L.Icon (svg to encoded base-64)</p></a>
+    <a href="./23-L.CircleMarker-extend.html"><p>L.circleMarker.extend</p></a>
+    <a href="./24-L.CircleMarker-custom.html"><p>L.circleMarker modify src</p></a>
+    <a href="./21-circle-markers.html"><p>L.circleMarker (leaflet mod)</p></a>
     `
     document.getElementById('list').innerHTML = html;  
 }
 function getRandomLatLng() {
 return [53.2 + .5 * Math.random(), -6 - .5 * Math.random()];
 }
+function getPoolbegLighthouse(){
+    return [53.342119, -6.151307];
+}
 
 // create the html list
 createList()
 
 // create the map and set the start point
-let map = L.map("map");
+let map = L.map("map", {customOption:'hello'});
 L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png").addTo(map);
 map.setView([53.35, -6.3], 10);
